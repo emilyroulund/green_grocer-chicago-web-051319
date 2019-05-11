48b388616638f9452d26f1477fe36713b04c24cd
Emily R <emilyroulund@gmail.com>
2019-05-11 17:26:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,30 +20,23 @@ def apply_coupons(cart, coupons)
  coupons.each do |coupon|
    name = coupon[:item]
    number = coupon[:num]
-   #does cart have same item in coupon, but larger amount that coupon? i.e. is cart[avocado][:count] > number 
+   #does cart have same item in coupon, but larger amount that coupon? i.e. is cart[avocado][:count] >= number 
+   if cart.include?(name) && cart[name][:count] >= number 
+     new_cart[name][:count] -= number #remove the new_cart count
+   if new_cart["#{name} W/COUPON"]
+     new_cart["#{name} W/COUPON"][:count] += 1 #INCREASE COUNT WHEN MORE ITEMS THAN coupons 
+   else 
+     new_cart["#{name} W/COUPON"] = {
+       :price =coupon[:cost],
+       :clearance => new_cart[name][:clearance],
+       :count =>1 
+     }
+   end
+ end
+ end
+ new_cart
 end
 
-    name = coupon[:item] #avocado, cheese,...
-    num_of_c = coupon[:num]
-    #if the cart has the same item in coupon and has larger amount than in coupon
-    if cart.include?(name) && cart[name][:count] >= num_of_c
-       #remove number of the new_cart's count
-       new_cart[name][:count] -= num_of_c
-       #increase the count when there is more items than the coupon allows
-       if new_cart["#{name} W/COUPON"]
-         new_cart["#{name} W/COUPON"][:count] += 1
-       #set the name with coupon with new value
-       else
-         new_cart["#{name} W/COUPON"] = {
-           :price => coupon[:cost],
-           :clearance => new_cart[name][:clearance],
-           :count => 1
-         }
-       end
-     end
-   end
-   new_cart
-end
 
 def apply_clearance(cart)
   # code here
